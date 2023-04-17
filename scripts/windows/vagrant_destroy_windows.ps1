72a611ca4fd56e0bcff29a47f85a2b8e6b1b7bad
marcos.silvestrini <marcos.silvestrini@gmail.com>
2023-04-17 09:04:01 -0300

    Add scripts windows

---
+++ scripts/windows/vagrant_destroy_windows.ps1
@@ -0,0 +1,103 @@
+<#
+.Synopsis
+   Destroy lab for learning
+.DESCRIPTION
+   This script is used for destroy lab with vagrant.
+   Destroy and delete all VM's in Vagrantfile
+   Delete all folders with VM's in Vagrantfile
+.EXAMPLE
+   & vagrant_destroy_windows.ps1
+#>
+
+#Stop vagrant process
+Get-Process -Name *vagrant* | Stop-Process -Force
+Get-Process -Name *ruby* | Stop-Process -Force
+
+#  define variables
+switch ($(hostname)) {
+    "silvestrini" {       
+        $vagrant = "E:\Apps\Vagrant\bin\vagrant.exe"
+        $baseVagrantfile="F:\CERTIFICACAO\labs\vagrant\"
+        $vagrantHome = "E:\Apps\Vagrant\vagrant.d"      
+        $virtualboxFolder = "E:\Apps\VirtualBox"
+        $virtualboxVMFolder = "E:\Servers\VirtualBox" 
+    }
+    "silvestrini2" {      
+        # Variables
+        $vagrant = "C:\Cloud\Vagrant\bin\vagrant.exe"       
+        $baseVagrantfile = "C:\Users\marcos.silvestrini\OneDrive\Projetos\labs\vagrant"
+        $vagrantHome = "C:\Cloud\Vagrant\.vagrant.d"      
+        $virtualboxFolder = "C:\Program Files\Oracle\VirtualBox"
+        $virtualboxVMFolder = "C:\Cloud\VirtualBox"
+    }
+    Default { Write-Host "This hostname is not available for execution this script!!!"; exit 1 }
+}
+
+# VirtualBox home directory.
+Start-Process -Wait -NoNewWindow -FilePath "$virtualboxFolder\VBoxManage.exe" `
+    -ArgumentList  @("setproperty", "machinefolder", "$virtualboxVMFolder")
+
+# Vagrant home directory for downloadad boxes.
+setx VAGRANT_HOME $vagrantHome >$null
+
+#Vagrant Boxes
+$bind = "$baseVagrantfile\linux\dnsbind"
+$http = "$baseVagrantfile\linux\http"
+$fs = "$baseVagrantfile\linux\fs"
+$dhcp = "$baseVagrantfile\linux\dhcp"
+$openldap = "$baseVagrantfile\linux\openldap"
+$postfix = "$baseVagrantfile\linux\postfix"
+$security = "$baseVagrantfile\linux\security"
+
+# Folder vagrant virtualbox machines artefacts
+$vmFolders = @(    
+    "$virtualboxVMFolder\ol9-server01",
+    "$virtualboxVMFolder\ol9-server02",
+    "$virtualboxVMFolder\debian-server01",
+    "$virtualboxVMFolder\debian-server02",    
+    "$virtualboxVMFolder\debian-client01"
+)
+
+#Destroy bind stack
+Set-Location $bind
+Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "destroy -f"  -Verb RunAs
+
+#Destroy http stack
+Set-Location $http
+Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "destroy -f"  -Verb RunAs
+
+#Destroy fs stack
+Set-Location $fs
+Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "destroy -f"  -Verb RunAs
+
+#Destroy dhcp_ldap stack
+Set-Location $dhcp
+Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "destroy -f"  -Verb RunAs
+
+#Destroy openldap stack
+Set-Location $openldap
+Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "destroy -f"  -Verb RunAs
+
+#Destroy postfix stack
+Set-Location $postfix
+Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "destroy -f"  -Verb RunAs
+
+#Destroy security stack
+Set-Location $security
+Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "destroy -f"  -Verb RunAs
+
+# Delete folder virtualbox machines artefacts
+$vmFolders | ForEach-Object {
+    If (Test-Path $_) {
+        If ( (Get-ChildItem -Recurse $_).Count -lt 3 ) {            
+            Remove-Item $_ -Recurse -Force
+        }        
+    }
+}
+
+# # Remove user vagrant with exist
+# $myuser="vagrant"
+# $op = Get-LocalUser | where-Object Name -eq $myuser | Measure
+# if ($op.Count -eq 1) {
+#     Remove-LocalUser -Name $myuser
+# }
---
+++ scripts/windows/vagrant_up_windows.ps1
@@ -0,0 +1,146 @@
+<#
+.Synopsis
+   Up lab for learning
+.DESCRIPTION
+   Set folder of virtualbox VM's
+   Create a semafore for vagrant up
+   Copy public key for vagrant shared folder
+   This script is used for create a new lab with vagrant.
+   Create all VM's in Vagrantfile  
+   Copy all private key of VM's for F:\Projetos\vagrant_pk folder   
+.EXAMPLE
+   & vagrant_up_windows.ps1
+#>
+
+# Clear screen
+Clear-Host
+
+#Stop vagrant process
+Get-Process -Name *vagrant* | Stop-Process -Force
+Get-Process -Name *ruby* | Stop-Process -Force
+
+# Semafore for vagrant process
+$scriptPath=$PSScriptRoot
+$semafore="$scriptPath\vagrant-up.silvestrini"
+New-Item -ItemType File -Path $semafore -Force >$null
+
+# SSH
+$ssh_path="$( (($scriptPath | Split-Path -Parent)| Split-Path -Parent) | Split-Path -Parent)\security"
+Copy-Item -Force "$env:USERPROFILE\.ssh\id_ecdsa.pub" -Destination $ssh_path
+
+switch ($(hostname)) {
+   "silvestrini" {
+      # Variables
+      $vagrant="E:\Apps\Vagrant\bin\vagrant.exe"
+      $vagrantHome = "E:\Apps\Vagrant\vagrant.d" 
+      $vagrantPK="F:\Projetos\vagrant-pk"
+      $baseVagrantfile="F:\CERTIFICACAO\labs\vagrant\"
+      $virtualboxFolder = "E:\Apps\VirtualBox"
+      $virtualboxVMFolder = "E:\Servers\VirtualBox"
+
+      # VirtualBox home directory.
+      Start-Process -Wait -NoNewWindow -FilePath "$virtualboxFolder\VBoxManage.exe" `
+      -ArgumentList  @("setproperty", "machinefolder", "$virtualboxVMFolder")
+      # Vagrant home directory for downloadad boxes.
+      setx VAGRANT_HOME "$vagrantHome" >$null
+   }
+   "silvestrini2" {      
+      # Variables
+      $vagrant="C:\Cloud\Vagrant\bin\vagrant.exe"
+      $vagrantHome = "C:\Cloud\Vagrant\.vagrant.d" 
+      $vagrantPK="C:\Cloud\Vagrant\vagrant-pk"
+      $baseVagrantfile="C:\Users\marcos.silvestrini\OneDrive\Projetos\labs\Vagrant"
+      $virtualboxFolder = "C:\Program Files\Oracle\VirtualBox"
+      $virtualboxVMFolder = "C:\Cloud\VirtualBox"
+
+      # VirtualBox home directory.
+      Start-Process -Wait -NoNewWindow -FilePath "$virtualboxFolder\VBoxManage.exe" `
+      -ArgumentList  @("setproperty", "machinefolder", "$virtualboxVMFolder")
+      # Vagrant home directory for downloadad boxes.
+      setx VAGRANT_HOME "$vagrantHome" >$null
+   }
+   Default {Write-Host "This hostname is not available for execution this script!!!";exit 1}
+}
+
+#Vagrant Boxes
+# $debian = "$baseVagrantfile\Debian"
+# $debian5 = "$baseVagrantfile\Debian5"
+# $ol9 = "$baseVagrantfile\OracleLinux"
+
+#up ol8
+# Set-Location $ol9
+# Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+# Copy-Item .\.vagrant\machines\ol9-lpic2-202\virtualbox\private_key $vagrantPK\oracle-linux9
+
+#up debian 11
+# Set-Location $debian
+# Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+# Copy-Item .\.vagrant\machines\debian_lpic2_202\virtualbox\private_key $vagrantPK\debian
+
+#up debian 5
+# Set-Location $debian5
+# Start-Process -Wait -WindowStyle Hidden  -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+# Copy-Item .\.vagrant\machines\debian5_lpic2_202\virtualbox\private_key $vagrantPK\debian5
+
+#Up Servers BIND
+$bind = "$baseVagrantfile\linux\dnsbind"
+Set-Location $bind
+Start-Process -Wait -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+Copy-Item .\.vagrant\machines\ol9-server01\virtualbox\private_key $vagrantPK\ol9-server01
+Copy-Item .\.vagrant\machines\debian-server01\virtualbox\private_key $vagrantPK\debian-server01
+Copy-Item .\.vagrant\machines\debian-server02\virtualbox\private_key $vagrantPK\debian-server02
+Copy-Item .\.vagrant\machines\ol9-server02\virtualbox\private_key $vagrantPK\ol9-server02
+Copy-Item .\.vagrant\machines\ol9-client01\virtualbox\private_key $vagrantPK\ol9-client01
+
+
+#Up Servers HTTP
+# $http = "$baseVagrantfile\HTTP"
+# Set-Location $http
+# Start-Process -Wait -WindowStyle Minimized -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+# Copy-Item .\.vagrant\machines\ol9-server01\virtualbox\private_key $vagrantPK\ol9-server01
+# Copy-Item .\.vagrant\machines\debian-server01\virtualbox\private_key $vagrantPK\debian-server01
+# Copy-Item .\.vagrant\machines\ol9-apache-ha\virtualbox\private_key $vagrantPK\ol9-apache-ha
+# Copy-Item .\.vagrant\machines\ol9-nginx-ha\virtualbox\private_key $vagrantPK\ol9-nginx-ha
+# Copy-Item .\.vagrant\machines\debian-apache-node01\virtualbox\private_key $vagrantPK\debian-apache-node01
+# Copy-Item .\.vagrant\machines\debian-apache-node02\virtualbox\private_key $vagrantPK\debian-apache-node02
+# Copy-Item .\.vagrant\machines\debian-http-client\virtualbox\private_key $vagrantPK\debian-http-client
+
+# Up Servers FILE SHARING
+# $fs = "$baseVagrantfile\FS"
+# Set-Location $fs
+# Start-Process -Wait -WindowStyle Minimized -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+# Copy-Item .\.vagrant\machines\ol9-server01\virtualbox\private_key $vagrantPK\ol9-server01
+# Copy-Item .\.vagrant\machines\debian-server01\virtualbox\private_key $vagrantPK\debian-server01
+# Copy-Item .\.vagrant\machines\debian-client01\virtualbox\private_key $vagrantPK\debian-client01
+
+# Up Servers DHCP,PAM, LDAP
+# $dhcp_ldap = "$baseVagrantfile\DHCP_LDAP"
+# Set-Location $dhcp_ldap
+# Start-Process -Wait -WindowStyle Minimized -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+# Copy-Item .\.vagrant\machines\ol9-server01\virtualbox\private_key $vagrantPK\ol9-server01
+# Copy-Item .\.vagrant\machines\debian-server01\virtualbox\private_key $vagrantPK\debian-server01
+# Copy-Item .\.vagrant\machines\debian-client01\virtualbox\private_key $vagrantPK\debian-client01
+
+
+# Up Servers Mail
+# $mail = "$baseVagrantfile\MAIL"
+# Set-Location $mail
+# Start-Process -Wait -WindowStyle Minimized -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+# Copy-Item .\.vagrant\machines\ol9-server01\virtualbox\private_key $vagrantPK\ol9-server01
+# Copy-Item .\.vagrant\machines\debian-server01\virtualbox\private_key $vagrantPK\debian-server01
+# Copy-Item .\.vagrant\machines\debian-client01\virtualbox\private_key $vagrantPK\debian-client01
+
+
+# $security = "$baseVagrantfile\SECURITY"
+# Set-Location $security
+# Start-Process -Wait -WindowStyle Minimized -FilePath $vagrant -ArgumentList "up"  -Verb RunAs
+# Copy-Item .\.vagrant\machines\ol9-server01\virtualbox\private_key $vagrantPK\ol9-server01
+# Copy-Item .\.vagrant\machines\ol9-server02\virtualbox\private_key $vagrantPK\ol9-server02
+# Copy-Item .\.vagrant\machines\debian-server01\virtualbox\private_key $vagrantPK\debian-server01
+# Copy-Item .\.vagrant\machines\debian-client01\virtualbox\private_key $vagrantPK\debian-client01
+
+#Fix powershell error
+$Env:VAGRANT_PREFER_SYSTEM_BIN += 0
+
+#Remove Semafore
+Remove-Item -Force $semafore
